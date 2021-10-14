1067bf8ba2a4ea383d85ad5632c23e1e574b568c
mash <mgso@foxmail.com>
2021-10-14 17:31:27 +0800

    feat:区分项目打包和预览器打包

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
+const target = process.env.TARGET || "";
 module.exports = {
+  outputDir: target === "lib" ? "previewer" : "dist",
   configureWebpack: {
     output: {
       libraryExport: "default",
